58d6483c9f3dfa5cfc4745383e8cb1569d91efd2
MOMO <mareimtarek2002@gmail.com>
2026-05-08 22:05:08 +0300

    Update metrics.py

--- project-root/src/src/metrics/metrics.py
+++ project-root/src/src/metrics/metrics.py
@@ -1,22 +1,23 @@
 def calculate_metrics(processes):
     metrics = {}
     n = len(processes)
-
+    
     if n == 0:
         return {}
-
+        
     total_wt = total_tat = total_rt = 0
-
+    
     for p in processes:
         tat = p["completion"] - p["arrival"]
         wt  = tat - p["burst"]
         rt  = p["start"] - p["arrival"]
-
+        
         metrics[p["id"]] = {
             "TAT": round(tat, 2),
             "WT" : round(wt,  2),
             "RT" : round(rt,  2),
         }
+        
         total_wt  += wt
         total_tat += tat
         total_rt  += rt
@@ -26,5 +27,4 @@ def calculate_metrics(processes):
         "WT" : round(total_wt  / n, 2),
         "RT" : round(total_rt  / n, 2),
     }
-
     return metrics
